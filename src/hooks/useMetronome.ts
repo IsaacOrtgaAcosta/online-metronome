34d6087f2e534f2a0f4cad871fab4ffee20de8eb
IsaacOrtgaAcosta <iortga.acosta@gmail.com>
2026-04-08 21:09:03 +0200

    feat: hook to control time created

---
+++ src/hooks/useMetronome.ts
@@ -0,0 +1,40 @@
+import { constants } from "buffer";
+import {useEffect, useRef, useState} from "react";
+
+const useMetronome = () => {
+const [isPlaying, setIsPlaying] = useState(false);
+const [bpm, setBpm] = useState(120);
+
+const intervalRef = useRef<NodeJS.Timeout | null>(null);
+
+const interval = (60 / bpm) * 1000;
+
+const tick = () => {
+    console.log('tick');
+}
+
+const start = () => {
+    if (intervalRef.current) return;
+
+    intervalRef.current = setInterval(tick, interval);
+    setIsPlaying(true);
+};
+
+const stop = () => {
+    if(intervalRef.current) {
+        clearInterval(intervalRef.current);
+        intervalRef.current = null;
+    }
+    setIsPlaying(false);
+}
+
+useEffect(() => {
+    if(isPlaying) {
+        stop();
+        start();
+    }
+}, [bpm]);
+
+return {isPlaying, bpm, setBpm, start, stop};
+
+} 
